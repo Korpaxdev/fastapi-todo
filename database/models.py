8f2739c3b02e6f81c4f0ecdc0ef1bb98b32bdd09
korpaxdev <korpaxdev@gmail.com>
2023-11-16 17:54:59 +0300

    add UserMode

--- database/models.py
+++ database/models.py
@@ -1,5 +1,17 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import String, Text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
-class BaseTable(DeclarativeBase):
+class BaseModel(DeclarativeBase):
     pass
+
+
+class UserModel(BaseModel):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(Text, nullable=False)
+
+    def __str__(self) -> str:
+        return f"User(email={self.email})"
--- migrations/env.py
+++ migrations/env.py
@@ -7,7 +7,7 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from database.config import DSN
-from database.models import BaseTable
+from database.models import BaseModel
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -23,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = BaseTable.metadata
+target_metadata = BaseModel.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
